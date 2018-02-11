6b7c0f1d81ca6dc28d8f3bdeb1757337fe23a8fd
Koh Meng Yit <kmykoh97@outlook.com>
2018-02-11 22:54:06 +0800

    Completed a game

--- Roll A Ball/Assets/scripts/playerController.cs
+++ Roll A Ball/Assets/scripts/playerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerController : MonoBehaviour {
 
     public float speed;
+    public Text scoreText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int score;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+        setScoreText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -22,4 +29,24 @@ public class playerController : MonoBehaviour {
 
         rb.AddForce(movement * speed);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            score += 1;
+            setScoreText();
+        }
+    }
+
+    void setScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 10)
+        {
+            winText.text = "Congratulations, you win!";
+        }
+    }
 }
+
